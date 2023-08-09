ad87547fdb50231b4d136d46cb2c64f6c5a4846d
TaeHyun <54016309+hhh88261@users.noreply.github.com>
2023-08-09 17:45:11 +0900

    Update views.py

--- AES/views.py
+++ AES/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from .encryption_module import SymmetricEncryption  # 암호화 모듈 임포트
-
+from django.shortcuts import render, redirect
+from .encryption_module import SymmetricEncryption
+from .models import EncryptedData
 
 def encrypt_data(request):
-    client_password = "user123"  # 사용자 비밀번호
+    client_password = "password"
     encryption_module = SymmetricEncryption(client_password.encode())
-
+    
     original_data = "Sensitive information."
     encrypted_data = encryption_module.encrypt(original_data)
-
-    return render(request, 'encrypted_data.html', {'encrypted_data': encrypted_data})
+    
+    EncryptedData.objects.create(encrypted_data=encrypted_data)
+    
+    return redirect('admin_page')
